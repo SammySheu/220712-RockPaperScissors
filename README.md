393ee0829ca1666509b4de3a35852b38fe6ea163
SammySheu <samsheu1997@gmail.com>
2022-07-28 16:20:44 +0800

    Add DOM concept in javascript

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 Establish on 220712, Finish on 220713.
 
 Adding UI funcion on 220727 14:28pm
+
+Add DOM concept in javascript at 16:19 on 220728
--- script.js
+++ script.js
@@ -7,9 +7,16 @@
 // Player Function
 let temp = 0, chosen_ply, chosen_com, point_ply=0, point_com=0;
 const buttons = document.querySelectorAll('button');
+let gameset = 0;
 // choose.onclick = () => console.log('Hello World!');      //Method 2
 buttons.forEach(button =>{
   button.addEventListener('click', (e) => {
+
+    const result_div = document.querySelector('.result');
+    const child = document.querySelector('.instant-result');
+    const history_u = document.querySelector('.history_u');
+    const history_d = document.querySelector('.history_d');
+
     // console.log(e.target.textContent);                                //Method 3
     chosen_com = computerPlay();
     chosen_ply = playerPlay( plySelection = e.target.textContent );
@@ -17,6 +24,12 @@ buttons.forEach(button =>{
     // console.log(chosen_ply);
     // console.log(chosen_ply);
     if (chosen_com == chosen_ply){
+      const hihi = document.querySelector('.determine');
+      result_div.removeChild(hihi);
+      const determine = document.createElement('div');
+      determine.textContent = 'Tied';
+      determine.classList = 'determine';
+      result_div.insertBefore(determine, history_u);
       // console.log("Tied!");
       // alert("Again");
       //     i--;
@@ -24,23 +37,56 @@ buttons.forEach(button =>{
     }
     else if( (chosen_ply-chosen_com == 1) | (chosen_com - chosen_ply == 2) ){
       point_ply++;
+      const hihi = document.querySelector('.determine');
+      result_div.removeChild(hihi);
+      const determine = document.createElement('div');
+      determine.textContent = 'You win';
+      determine.classList = 'determine';
+      result_div.insertBefore(determine, history_u);
       // console.log('Player win');
     }
     else{
       point_com++;
+      const hihi = document.querySelector('.determine');
+      result_div.removeChild(hihi);
+      const determine = document.createElement('div');
+      determine.textContent = 'Computer win';
+      determine.classList = 'determine';
+      result_div.insertBefore(determine, history_u);
       // console.log('Computer win');
     }
     console.log(point_com, point_ply);
+
+
+    result_div.removeChild(child);
+    display = document.createElement('div');
+    // display.setAttribute('class', 'instant-result');
+    display.classList.add('instant-result');
+    display.textContent = "(Instant Score)" + point_com + ":" + point_ply;
+    result_div.insertBefore(display, history_u);
+
     if(point_com>=5){
+      gameset++;
+      const judge = document.createElement('div');
+      judge.textContent = "Game" + gameset + ": Computer win!!";
+      history_u.insertBefore(judge,history_d);
       console.log('Computer win');
       point_com=0;
       point_ply=0;
     }
     else if(point_ply>=5){
+      gameset++;
+      const judge = document.createElement('div');
+      const history_u = document.querySelector('.history_u');
+      const history_d = document.querySelector('.history_d');
+      judge.textContent = "Game" + gameset + ": Player win!!";
+      history_u.insertBefore(judge,history_d);
       console.log('Player win');
       point_com=0;
       point_ply=0;
     }
+
+
   })
 })
 
@@ -72,6 +118,11 @@ let computerPlay = () =>{
     return 2;
   }
 }
+
+
+
+
+
 
 
 // chosen_com = computerPlay();
